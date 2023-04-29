ec92c460a43c7b0b289923554893af0fa5b9ff09
saleempirwani <msr.pirwani@gmail.com>
2023-04-30 02:30:17 +0500

    relocate

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { auth } = require("../middleware/auth");
+const BOOKS = require("../data/books.json");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT | 3000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "Unauthorized" });
+});
+
+app.get("/books", (req, res) => {
+  res.send({ data: BOOKS, message: "Successfully fetch" });
+});
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
