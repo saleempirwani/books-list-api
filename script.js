a0a4cce57d087358bbd14c11160233345551ce3d
saleempirwani <msr.pirwani@gmail.com>
2023-04-30 02:43:03 +0500

    updated

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ const BOOKS = require("./data/books1.json");
 const IMAGES = require("./data/images.json");
 
 const data = [];
+const imageScript = () => {
+  for (let i = 0; i < BOOKS.length; i++) {
+    let book = BOOKS[i];
 
-for (let i = 0; i < BOOKS.length; i++) {
-  let book = BOOKS[i];
+    for (let j = 0; j < IMAGES.length; j++) {
+      const image = IMAGES[j];
+      let image_name = image.slice(image.lastIndexOf("/") + 1, image.length);
 
-  for (let j = 0; j < IMAGES.length; j++) {
-    const image = IMAGES[j];
-    let image_name = image.slice(image.lastIndexOf("/") + 1, image.length);
-
-    if (book.imageLink === image_name) {
-      //   console.log();
-      book = { ...book, imageLink: image };
-      data.push(book);
+      if (book.imageLink === image_name) {
+        //   console.log();
+        book = { ...book, imageLink: image };
+        data.push(book);
+      }
     }
   }
-}
 
-console.log("first", JSON.stringify(data));
+  console.log("first", JSON.stringify(data));
+};
