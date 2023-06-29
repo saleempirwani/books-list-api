495ac80854ca64f423cda8b8362b5bbbba6cc0f2
saleempirwani <msr.pirwani@gmail.com>
2023-06-29 19:35:49 +0500

    updated

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const BOOKS = require("./data/books1.json");
+const fs = require("fs");
+const BOOKS1 = require("./data/books1.json");
+const BOOKS2 = require("./data/books2.json");
 const IMAGES = require("./data/images.json");
 
 const data = [];
+
 const imageScript = () => {
-  for (let i = 0; i < BOOKS.length; i++) {
-    let book = BOOKS[i];
+  for (let i = 0; i < BOOKS1.length; i++) {
+    let book = BOOKS1[i];
 
     for (let j = 0; j < IMAGES.length; j++) {
       const image = IMAGES[j];
@@ -20,3 +23,19 @@ const imageScript = () => {
 
   console.log("first", JSON.stringify(data));
 };
+
+const addPropertiesToData = () => {
+  for (let i = 0; i < BOOKS2.length; i++) {
+    const properties = {
+      price: Math.round(Math.random() * 100),
+      rating: Math.round(Math.random() * 5),
+      reviews: Math.round(Math.random() * 500),
+    };
+
+    const item = { ...BOOKS2[i], ...properties };
+    data.push(item);
+  }
+};
+
+addPropertiesToData();
+fs.writeFileSync("./data/books.json", JSON.stringify(data));
